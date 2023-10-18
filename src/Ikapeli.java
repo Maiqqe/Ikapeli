f1b333ad5dc88e2923d967606bc6b1b48893ec48
Maiqqe <maisa.tuomenpuro@student.hamk.fi>
2023-10-18 18:37:13 +0300

    Tiedoston luonti ja tehtävänanto

---
+++ src/Ikapeli.java
@@ -0,0 +1,17 @@
+
+public class Ikapeli {
+
+
+	public static void main(String[] args) {
+		/* 
+Tehtävä 1: Laajenna ohjelmaa siten, että ohjelma osaa kertoa 16-17-vuotiaille, että he voivat ajaa kevaria.
+Tehtävä 2: Laajenna ohjelmaa niin, että se kertoo 18-vuotiaalle, että hän on juuri tullut täysi-ikäiseksi ja saa ajaa autoa. 
+Tehtävä 3: Laajenna tehtävää niin, että aikuisille ilmoitetaan tasavuosikymmenistä onnentoivotus. 
+Tehtävä 4: jos ikä on 100, tulosta 3-rivinen onnentoivotus. 
+Tehtävä 5: Tarkenna eläkeasioita siten, että yli 58-vuotiaille kerro, että he voivat mennä varhaiseläkkeelle. 
+Tehtävä 6: Toivota 65-vuotiaille hyviä eläkepäiviä.
+
+Tehtävä 7: 40–50 -vuotiaille toivota parasta keski-ikää.*/
+	}
+
+}
